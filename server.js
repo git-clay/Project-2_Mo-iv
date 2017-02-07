039d8677fc0613a2b794fab68c1c31f52dc3a4b5
git-clay <ce.smith5525@gmail.com>
2017-02-07 11:28:45 -0700

    removed apicall on server.js

--- server.js
+++ server.js
@@ -52,64 +52,64 @@ app.use(routes);
 /*********************** Daily api call  ******************************/
 
 
-// var date 	= new Date(),
-// 	dd 		= date.getDate()-1,
-// 	mm 		= date.getMonth()+1,
-// 	yyyy	= date.getFullYear(),
-// 	rule	= new schedule.RecurrenceRule(),
-// 	url		= 'http://hedonometer.org/data/shifts/world/',
-// 	shift 	= '-shift.csv',
-// 	metashift = '-metashift.csv';
+var date 	= new Date(),
+	dd 		= date.getDate()-1,
+	mm 		= date.getMonth()+1,
+	yyyy	= date.getFullYear(),
+	rule	= new schedule.RecurrenceRule(),
+	url		= 'http://hedonometer.org/data/shifts/world/',
+	shift 	= '-shift.csv',
+	metashift = '-metashift.csv';
 
-// rule.dayOfWeek =[0, new schedule.Range(0,7)];
-// rule.hour = 0;
-// //used to schedule the api call
+rule.dayOfWeek =[0, new schedule.Range(0,7)];
+rule.hour = 0;
+//used to schedule the api call
 // var apiCall = schedule.scheduleJob(rule,function(){ 
 // 	console.log('ran', date)
 // 	getHedoData();
 // });
-// // console.log('date:',date,'rule:',rule,'schedule',schedule)
+// console.log('date:',date,'rule:',rule,'schedule',schedule)
 
-// if(dd<10) {dd='0'+dd;} 
-// if(mm<10) {mm='0'+mm;} 
-// date =	yyyy+'-'+ mm +'-'+dd;
+if(dd<10) {dd='0'+dd;} 
+if(mm<10) {mm='0'+mm;} 
+date =	yyyy+'-'+ mm +'-'+dd;
 
-// var getShift = url+date+shift,
-// 	getMetashift = url+date+metashift,
-// 	storeObj={};
+var getShift = url+date+shift,
+	getMetashift = url+date+metashift,
+	storeObj={};
 
-// function getCsv(url,callback){
-// 	httpRequest.get(url, function (err, res, body) {
-// 	    if (!err && res.statusCode == 200) {
-// 	        callback(body);
-// 	    }
-// 	})
-// }
+function getCsv(url,callback){
+	httpRequest.get(url, function (err, res, body) {
+	    if (!err && res.statusCode == 200) {
+	        callback(body);
+	    }
+	})
+}
 
-// function babyParseFunc (url,which) { 
-// 	getCsv(url,function(res){
-// 	var parsed = Baby.parse(res).data;
-// 		if(which=='shift'){
-// 		storeObj.shift=parsed;
-// 		console.log(typeof storeObj.shift)
-// 		} else if(which=='meta'){
-// 		storeObj.meta=parsed;
-// 		console.log(typeof storeObj.meta)
-// 		}
-// 	if(storeObj.shift!== undefined &&storeObj.meta!== undefined){
-// 	storeObj.date = date;
-// 		console.log(storeObj)
-// 	db.DailyInfo.create(storeObj)
-// 		}
-// 	});
-// }
+function babyParseFunc (url,which) { 
+	getCsv(url,function(res){
+	var parsed = Baby.parse(res).data;
+		if(which=='shift'){
+		storeObj.shift=parsed;
+		console.log(typeof storeObj.shift)
+		} else if(which=='meta'){
+		storeObj.meta=parsed;
+		console.log(typeof storeObj.meta)
+		}
+	if(storeObj.shift!== undefined &&storeObj.meta!== undefined){
+	storeObj.date = date;
+		console.log(storeObj)
+	db.DailyInfo.create(storeObj)
+		}
+	});
+}
 
 
-// function getHedoData(){
-// 	babyParseFunc(getShift,'shift') //babyfunc -> getCsv = returned
-// 	babyParseFunc(getMetashift,'meta')
-// }
-// getHedoData();
+function getHedoData(){
+	babyParseFunc(getShift,'shift') //babyfunc -> getCsv = returned
+	babyParseFunc(getMetashift,'meta')
+}
+getHedoData();
 /*********************** SERVER ******************************/
 app.listen(process.env.PORT || 3000, function() {
 	console.log('BOOM, Express is firing on all cylinders');
